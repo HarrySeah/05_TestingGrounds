7c876c8df8807a5a749af14c959957b3a3855bc5
HARRYSEAH\Harry <harry_seah@hotmail.com>
2017-07-18 20:31:39 +0800

    Section 5, Lecture 250: Attach Gun to Player
    See the CPP code ( Is to attach the Gun to player)
    
    Set FPC BP of details Gun BP to Gun
    
    Gun BP off the owner see only

--- Source/S05_TestingGround/Player/FirstPersonCharacter.cpp
+++ Source/S05_TestingGround/Player/FirstPersonCharacter.cpp
@@ -8,7 +8,7 @@
 #include "Components/InputComponent.h"
 #include "GameFramework/InputSettings.h"
 #include "Kismet/HeadMountedDisplayFunctionLibrary.h"
-
+#include "../Weapons/Gun.h"
 #include "MotionControllerComponent.h"
 
 DEFINE_LOG_CATEGORY_STATIC(LogFPChar, Warning, All);
@@ -78,8 +78,15 @@ void AFirstPersonCharacter::BeginPlay()
 	// Call the base class  
 	Super::BeginPlay();
 
+	if (GunBlueprint == NULL)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Gun blueprint missing."));
+		return;
+	}
+
 	//Attach gun mesh component to Skeleton, doing it here because the skeleton is not yet created in the constructor
-	//FP_Gun->AttachToComponent(Mesh1P, FAttachmentTransformRules(EAttachmentRule::SnapToTarget, true), TEXT("GripPoint"));
+	Gun = GetWorld()->SpawnActor<AGun>(GunBlueprint);
+	Gun->AttachToComponent(Mesh1P, FAttachmentTransformRules(EAttachmentRule::SnapToTarget, true), TEXT("GripPoint"));
 
 	// Show or hide the two versions of the gun based on whether or not we're using motion controllers.
 	if (bUsingMotionControllers)
